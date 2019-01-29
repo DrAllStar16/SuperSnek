5205073f075e91e6f2cf6572f087e6def0fd5d97
DrAllStar16 <league@WTS14.attlocal.net>
2019-01-28 16:57:15 -0800

    I worked on GP

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,16 +1,59 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
+	Timer t;
+	final static int MENU_STATE = 0;
+	final static int GAME_STATE = 1;
+	final static int END_STATE = 2;
+	public static int currentState = MENU_STATE;
 
 	public GamePanel() {
-
+		t = new Timer(1000 / 60, this);
 	}
 
 	public void paintComponent(Graphics g) {
 		g.setColor(new Color(0, 150, 70));
 		g.fillRect(0, 0, 100, 100);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (currentState == MENU_STATE) {
+
+			updateMenuState();
+
+		} else if (currentState == GAME_STATE) {
+
+			updateGameState();
+
+		} else if (currentState == END_STATE) {
+
+			updateEndState();
+
+		}
+		repaint();
+
+	}
+
+	private void updateMenuState() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void updateGameState() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void updateEndState() {
+		// TODO Auto-generated method stub
+
 	}
 }
